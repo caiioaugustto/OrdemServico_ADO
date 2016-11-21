fa3fb2cc7a86524ff2e91a0112f1d2e0b8d1c8b5
Caio Augusto <caiio_augustto@yahoo.com.br>
2016-11-21 17:51:48 -0200

    Exportação em Excel na tela de Fornecedor

--- OrdemServico_ADO/Controllers/FornecedorController.cs
+++ OrdemServico_ADO/Controllers/FornecedorController.cs
@@ -1,10 +1,15 @@
 ﻿using Entidades;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Uteis;
+
 
 namespace Controllers
 {
@@ -60,6 +65,65 @@ namespace Controllers
         {
             var fornecedor = fornRepo.ListarFiltro(nome, email);
             return RedirectToAction("Index", "Fornecedor", fornecedor);
+        }
+
+        public ActionResult Exportar()
+        {
+            IEnumerable<Fornecedor> fornecedores = fornRepo.Listar();
+
+            //fornecedores = AplicarFiltros(fornecedores, ddlLinha, ddlStatus, ddlStatusVaAtivo, txtNome).OrderBy(p => p.Nome);
+
+            MemoryStream stream = new MemoryStream();
+
+            using (ExcelPackage xlPackage = new ExcelPackage())
+            {
+                ExcelWorksheet ws = xlPackage.Workbook.Worksheets.Add("Produtos");
+                var numeroLinha = 2;
+
+                //fornecedores = fornecedores.Where(p => p.ProdutoVas.Any(va => va.Status == (char)ProdutoVa.EnumStatus.Ativo));
+
+                ws.Cells[1, 1].Value = "Nome";
+                ws.Cells[1, 2].Value = "Nome Responsavel";
+                ws.Cells[1, 3].Value = "Número Fornecedor";
+                ws.Cells[1, 4].Value = "Descrição";
+                ws.Cells[1, 5].Value = "Email";
+                ws.Cells[1, 6].Value = "Telefone";
+                
+                ws.Cells[1, 1, 1, 6].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                ws.Cells[1, 1, 1, 6].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml("#0000CD"));
+                ws.Cells[1, 1, 1, 6].Style.Font.Bold = true;
+                ws.Cells[1, 1, 1, 6].Style.Font.Size = 14;
+                ws.Cells[1, 1, 1, 6].Style.Font.Color.SetColor(ColorTranslator.FromHtml("#FFFFFF"));
+
+                foreach (var fornecedor in fornecedores)
+                {
+                    ws.Cells[numeroLinha, 1].Value = fornecedor.Nome;
+                    ws.Cells[numeroLinha, 2].Value = fornecedor.NomeResponsavel;
+                    ws.Cells[numeroLinha, 3].Value = fornecedor.NumeroFornecedor;
+                    ws.Cells[numeroLinha, 4].Value = fornecedor.Descricao;
+                    ws.Cells[numeroLinha, 5].Value = fornecedor.Email;
+                    ws.Cells[numeroLinha, 6].Value = fornecedor.Telefone;
+
+                    numeroLinha++;
+                }
+
+                ws.Cells[1, 1, numeroLinha, 6].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                ws.Cells[1, 1, numeroLinha, 6].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                ws.Cells[1, 1, numeroLinha, 6].Style.WrapText = true;
+
+                ws.Column(1).Width = 40;
+                ws.Column(2).Width = 40;
+                ws.Column(3).Width = 40;
+                ws.Column(4).Width = 30;
+                ws.Column(5).Width = 50;
+                ws.Column(6).Width = 20;
+
+                xlPackage.Workbook.Properties.Company = "Exportação Excel";
+
+                stream = new MemoryStream(xlPackage.GetAsByteArray());
+            }
+
+            return File(stream, "application/xls", "Fornecedores.xlsx");
         }
     }
 }
